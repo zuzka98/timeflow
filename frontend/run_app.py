2f65261b46cbbbe404e0cd6b93e79d9f5b9fef24
liberty-rising <rwyszomirski@dyvenia.com>
2022-04-07 13:03:14 +0200

    configure fastapi server

--- frontend/run_app.py
+++ frontend/run_app.py
@@ -1,10 +1,15 @@
 import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
 from idom.server import fastapi
+from idom.server.starlette import Options
+from pathlib import Path
 from index import page as index_page
 
 app = fastapi.create_development_app()
-
-fastapi.configure(app, index_page)
+HERE = Path(__file__).parent
+app.mount("/static", StaticFiles(directory=str(HERE)), name="static")
+fastapi.configure(app, index_page, Options(redirect_root=False))
 
 
 def run():
