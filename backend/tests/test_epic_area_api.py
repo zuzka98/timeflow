a7c9d94c9235eedf30095429f39164b3b6062324
jkrobicki <jkrobickir@dyvenia.com>
2022-04-21 12:56:41 +0200

    update epic_areas

--- backend/tests/test_epic_area_api.py
+++ backend/tests/test_epic_area_api.py
@@ -15,57 +15,47 @@ def test_post_epic_area(client):
             "epic_id": 1,
             "name": "graphics",
             "is_active": True,
+            "created_at": "2022-04-21T08:57:31.591Z",
+            "updated_at": "2022-04-21T08:57:31.591Z"
         },
     )
     data = response.json()
     assert response.status_code == 200
     assert data == {
-        "id": 1,
-        "epic_id": 1,
-        "name": "graphics",
-        "is_active": True,
-    }
+            "id":1,
+            "epic_id": 1,
+            "name": "graphics",
+            "is_active": True,
+            "created_at": "2022-04-21T08:57:31.591000",
+            "updated_at": "2022-04-21T08:57:31.591000"
+        }
 
-
-def test_read_epic_areas(client):
-    response = client.get("/api/epics/graphics")
-    data = response.json()
-    assert response.status_code == 200
-    assert data == {
-        "id": 1,
-        "epic_id": 1,
-        "name": "graphics",
-        "is_active": True,
-    }
-
-
-def test_update_epic_area(client):
-    response = client.put("api/epics/?id=1&name=graphics&is_active=True&new_epic_id=2")
-    data = response.json()
-    assert response.status_code == 200
-    assert data == {
-        "id": 1,
-        "epic_id": 2,
-        "name": "graphics",
-        "is_active": True,
-    }
-
-
-def test_get_epic_list(client):
+def test_get_epic_areas_list(client):
     response = client.get("/api/epic_areas/")
     data = response.json()
     assert response.status_code == 200
     assert data == [
         {
-            "id": 1,
-            "epic_id": 2,
+            "id":1,
+            "epic_id": 1,
             "name": "graphics",
             "is_active": True,
+            "created_at": "2022-04-21T08:57:31.591000",
+            "updated_at": "2022-04-21T08:57:31.591000"
         }
     ]
 
-
-@pytest.mark.order(-1)
-def test_deactivate_epics(client):
+# @pytest.mark.order(-1)
+def test_deactivate_epic_area(client):
     response = client.put("/api/epic_areas/graphics/deactivate")
     assert response.status_code == 200
+
+def test_activate_epic_area(client):
+    response = client.put("api/epic_areas/graphics/activate")
+    assert response.status_code == 200
+
+def test_update_epic_area(client):
+    response = client.put("/api/epic_areas/?id=1&new_name=graphics&is_active=True&new_epic_id=2")
+    data = response.json()
+    assert response.status_code == 200
+
