d8b96a3a98765c007e6d5e0099b9106662da7fb8
jkrobicki <jkrobickir@dyvenia.com>
2022-04-27 10:58:43 +0200

    main session refactor

--- backend/main.py
+++ backend/main.py
@@ -10,6 +10,7 @@ from .utils import (
     create_db,
     tags_metadata,
     execute_sample_sql,
+    get_session
 )
 from .api import (
     user,
@@ -29,7 +30,6 @@ import csv
 
 app = FastAPI(title="timeflow app API", openapi_tags=tags_metadata)
 
-session = Session(engine)
 app.include_router(timelog.router)
 app.include_router(forecast.router)
 app.include_router(user.router)
@@ -46,38 +46,43 @@ app.include_router(demand.router)
 
 @app.on_event("startup")
 def on_startup():
-    if os.getenv("TIMEFLOW_DEV") == "true":
-        create_db()
-        execute_sample_sql(session)
-    elif os.getenv("TIMEFLOW_DEV") == "false":
-        try:
-            statement = select(TimeLog)
-            results = session.exec(statement)
-        except ProgrammingError:
-            create_db()
+    with Session(engine) as session:
+        if os.getenv("TIMEFLOW_DEV") == "true":
+            try:
+                statement = select(TimeLog)
+                results = session.exec(statement)
+            except ProgrammingError:
+                create_db()
+                execute_sample_sql()
+        elif os.getenv("TIMEFLOW_DEV") == "false":
+            try:
+                statement = select(TimeLog)
+                results = session.exec(statement)
+            except ProgrammingError:
+                create_db()
+
 
 
 @app.on_event("startup")
 def implement_calendar_table():
-    try:
-        statement = select(Calendar.year_name).where(Calendar.id == 1)
-        result = session.exec(statement).one()
-    except Exception as e:
-        print(e)
-        values_sql = f"""INSERT INTO app_db.calendar (date, year_number, year_name, quarter_number, quarter_name
-                    , month_number, month_name, week_number, week_name, week_day_number, week_day_name)
-                    VALUES """
-        with open("backend/calendar.csv") as csvfile:
-            reader = csv.reader(csvfile, delimiter=",", quotechar="|")
-            values_list = []
-            for index, row in enumerate(reader):
-                if index > 0 and row[0] != "":
-                    _row = [f"'{item}'" for item in row]
-                    row_sql = ", ".join(_row)
-                    values = f"({row_sql}),"
-                    values_sql += values
-            values_sql += f"({row_sql});"
-            with engine.connect() as con:
-                con.execute(text(values_sql))
-                con.commit()
-                con.close()
+    with Session(engine) as session:
+        try:
+            statement = select(Calendar.year_name).where(Calendar.id == 1)
+            result = session.exec(statement).one()
+        except Exception as e:
+            print(e)
+            values_sql = f"""INSERT INTO app_db.calendar (date, year_number, year_name, quarter_number, quarter_name
+                        , month_number, month_name, week_number, week_name, week_day_number, week_day_name)
+                        VALUES """
+            with open("backend/calendar.csv") as csvfile:
+                reader = csv.reader(csvfile, delimiter=",", quotechar="|")
+                values_list = []
+                for index, row in enumerate(reader):
+                    if index > 0 and row[0] != "":
+                        _row = [f"'{item}'" for item in row]
+                        row_sql = ", ".join(_row)
+                        values = f"({row_sql}),"
+                        values_sql += values
+                values_sql += f"({row_sql});"
+                session.execute(text(values_sql))
+                session.commit()
